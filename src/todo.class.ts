4bdd810ca6922a0627a5d28095cb6fb4681099e5
Jan Elznic <jan@elznic.com>
2024-02-28 21:59:03 +0100

    TodoList - Basic management

--- src/todo.class.ts
+++ src/todo.class.ts
@@ -1,5 +1,17 @@
+import { Task } from 'task.interface';
+
 class TodoList {
-  constructor() {
-    console.log('Ahoj');
+  private tasks: Task[] = [];
+
+  addTask(task: Task) {
+    this.tasks.push(task);
+  }
+
+  removeTaskById(id: number) {
+    this.tasks = this.tasks.filter((todo) => todo.id !== id);
+  }
+
+  getTask(): Task[] {
+    return this.tasks;
   }
 }
