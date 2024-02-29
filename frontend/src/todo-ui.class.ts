fc78b331b379c46eb01d0d4dbb399ee237f6d37d
Jan Elznic <jan@elznic.com>
2024-02-29 03:00:10 +0100

    Frontend - API implementation, Edit form, new styles

--- frontend/src/todo-ui.class.ts
+++ frontend/src/todo-ui.class.ts
@@ -3,31 +3,62 @@ import { TodoList } from './todo.class';
 
 export class TodoListUI {
   private todoList: TodoList;
-  private elm: { todoList, addItemField, addButton };
+  private editableTask: Task;
+  private elm: { addForm, addItemField, addButton, editForm, editItemField, saveButton, cancelButton, todoList };
 
   constructor(todoList: TodoList) {
     this.todoList = todoList;
 
     this.elm = {
       todoList: document.getElementById('todoList')!,
+      editForm: document.getElementById('editForm')!,
+      editItemField: document.getElementById('editItemField') as HTMLInputElement,
+      saveButton: document.getElementById('saveButton')!,
+      cancelButton: document.getElementById('cancelButton')!,
+      addForm: document.getElementById('addForm')!,
       addItemField: document.getElementById('addItemField') as HTMLInputElement,
       addButton: document.getElementById('addButton')!
     };
 
-    this.buildDOM();
     this.addEventListeners();
   }
 
-  buildDOM() {
-    this.elm.todoList.innerHTML = '';
+  addEventListeners() {
+    this.elm.addButton.addEventListener('click', () => this.addTask());
+    this.elm.saveButton.addEventListener('click', () => this.saveTask());
+    this.elm.cancelButton.addEventListener('click', () => this.swhitchToAddForm());
 
-    this.todoList.getTasks().forEach((todo, index) => {
+    document.addEventListener('click', (event) => {
+      const target = event.target as HTMLElement;
+      const id = parseInt(target.getAttribute('data-id')!);
+
+      if (target.matches('.remove-button')) {
+        this.removeTaskById(id);
+      }
+      if (target.matches('.update-button')) {
+        this.updateTaskById(id);
+      }
+      if (target.matches('.check-button')) {
+        this.checkTaskById(id);
+      }
+    });
+
+    window.addEventListener('load', () => {
+      this.buildDOM();
+    });
+  }
+
+  async buildDOM() {
+    const tasks = await this.todoList.getTasks();
+    this.elm.todoList.innerHTML = '';
+    tasks.forEach((todo, index) => {
       const newTaskElm = document.createElement('li');
       newTaskElm.innerHTML = `
-        <label for="item_${index}" class="item-label">
+        <label for="item_${index}" class="item-label check-button" data-id="${todo.id}">
           <input type="checkbox" ${todo.completed ? "checked" : ""} id="item_${index}">
           <span>${todo.text}</span>
         </label>
+        <button class="update-button" data-id="${todo.id}">Update</button>
         <button class="remove-button" data-id="${todo.id}">Remove</button>
       `;
 
@@ -35,24 +66,11 @@ export class TodoListUI {
     });
   }
 
-  addEventListeners() {
-    this.elm.addButton.addEventListener('click', () => this.addTodo());
-
-    document.addEventListener('click', (event) => {
-      const target = event.target as HTMLElement;
-      if (target.matches('.remove-button')) {
-        const id = parseInt(target.getAttribute('data-id')!);
-        this.removeTodoById(id);
-      }
-    });
-  }
-
-  addTodo() {
+  addTask() {
     const text = this.elm.addItemField.value.trim();
 
     if (text) {
       const newTodo: Task = {
-        id: Date.now(),
         text: text,
         completed: false,
       };
@@ -63,8 +81,42 @@ export class TodoListUI {
     }
   }
 
-  removeTodoById(id: number) {
-    this.todoList.removeTaskById(id);
+  async removeTaskById(id: number) {
+    await this.todoList.removeTaskById(id);
     this.buildDOM();
+  }
+
+  async checkTaskById(id: number) {
+    let task = await this.todoList.getTaskById(id);
+    task.completed = !task.completed;
+    await this.todoList.updateTaskById(id, task);
+    this.buildDOM();
+  }
+
+  async updateTaskById(id: number) {
+    this.editableTask = await this.todoList.getTaskById(id);
+    this.elm.editItemField.value = this.editableTask.text;
+    this.switchToEditForm();
+  }
+
+  async saveTask() {
+    await this.todoList.updateTaskById(this.editableTask.id, {
+      text: this.elm.editItemField.value,
+      completed: this.editableTask.completed
+    });
+    this.buildDOM();
+    this.swhitchToAddForm();
+  }
+
+  switchToEditForm() {
+    this.elm.addForm.style.display = 'none';
+    this.elm.editForm.style.display = 'block';
+  }
+
+  swhitchToAddForm() {
+    this.editableTask = null;
+    this.elm.editItemField.value = '';
+    this.elm.addForm.style.display = 'block';
+    this.elm.editForm.style.display = 'none';
   }
 }
--- frontend/src/todo.class.ts
+++ frontend/src/todo.class.ts
@@ -2,20 +2,41 @@ import { Task } from './task.interface';
 
 export class TodoList {
   private tasks: Task[] = [];
+  private backendUrl: string;
 
-  constructor(tasks: Task[]) {
-    this.tasks = tasks;
+  constructor(backendUrl: string) {
+    this.backendUrl = backendUrl;
   }
 
-  addTask(task: Task) {
-    this.tasks.push(task);
+  async addTask(task: Task) {
+    const res = await fetch(`${this.backendUrl}/tasks/`, {
+      method: 'POST',
+      body: JSON.stringify(task),
+      headers: { "Content-Type": "application/json" }
+    });
+    console.log(res.json());
+    return res;
   }
 
-  removeTaskById(id: number) {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+  async updateTaskById(id: number, task) {
+    return await fetch(`${this.backendUrl}/tasks/${id}`, {
+        method: 'PATCH',
+        body: JSON.stringify(task),
+        headers: { "Content-Type": "application/json" }
+    });
   }
 
-  getTasks(): Task[] {
-    return this.tasks;
+  async removeTaskById(id: number) {
+    return await fetch(`${this.backendUrl}/tasks/${id}`, { method: 'DELETE' });
+  }
+
+  async getTaskById(id: number) {
+    const res = await fetch(`${this.backendUrl}/tasks/id/${id}`);
+    return await res.json();
+  }
+
+  async getTasks(): Promise<Task[]> {
+    const res = await fetch(`${this.backendUrl}/tasks/`);
+    return this.tasks = await res.json();
   }
 }
